394374fbf277997daf4a430741a9483550bb0fe1
timw <haodoan@datastreamsglobal.com>
2025-05-13 11:59:19 +0900

    Update build options, prepare for CI/CD

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,9 @@ import { defineConfig } from "vite";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import fs from "node:fs";
-import * as esbuild from "esbuild";
 
 // https://vite.dev/config/
-const sourceJSPattern = /\/src\/.*\.js$/;
-const rollupPlugin = (matchers) => ({
-	name: "js-in-jsx",
-	load(id) {
-		if (matchers.some((matcher) => matcher.test(id))) {
-			const file = fs.readFileSync(id, { encoding: "utf-8" });
-			return esbuild.transformSync(file, { loader: "jsx" });
-		}
-	},
-});
+
 
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
@@ -23,19 +12,6 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(__dirname, "src"),
 		},
-	},
-	build: {
-		rollupOptions: {
-			plugins: [rollupPlugin([sourceJSPattern])],
-		},
-		commonjsOptions: {
-			transformMixedEsModules: true,
-		},
-	},
-	esbuild: {
-		loader: "jsx",
-		include: [sourceJSPattern],
-		exclude: [],
 	},
 	optimizeDeps: {
 		esbuildOptions: {

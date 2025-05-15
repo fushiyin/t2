1aa49a0ede787e4f8005d1c4839f126502cbb481
htnnth <trucongftu@gmail.com>
2025-05-15 23:44:55 +0700

    initilaize

--- src/views/Home/components/CompetitiveEdges.jsx
+++ src/views/Home/components/CompetitiveEdges.jsx
@@ -5,9 +5,9 @@ const CompetitiveEdges = () => {
 		{
 			id: 0,
 			title: "Korean PM Coordinator",
-			icon: <Cpu className="w-12 h-12 text-white" />,
+			icon: <Cpu className="w-12 h-12 text-[#0d1b3e]" />,
 			bgColor: "bg-[#0d1b3e]",
-			textColor: "text-white",
+			textColor: "text-[#0d1b3e]",
 			borderColor: "border-[#0d1b3e]",
 			hover: "hover:bg-linear-to-r from-[#00A9C0] to-[#7CD957] hover:text-white transition-colors duration-300 ease-in-out",
 			isActive: true,
@@ -21,9 +21,9 @@ const CompetitiveEdges = () => {
 		{
 			id: 1,
 			title: "Skilled developers",
-			icon: <Code className="w-12 h-12 text-white" />,
+			icon: <Code className="w-12 h-12 text-[#0d1b3e]" />,
 			bgColor: "bg-white",
-			textColor: "text-white",
+			textColor: "text-[#0d1b3e]",
 			borderColor: "border-white",
 			hover: "hover:bg-linear-to-r from-[#00A9C0] to-[#7CD957] hover:text-white transition-colors duration-300 ease-in-out",
 			isActive: false,
@@ -37,9 +37,9 @@ const CompetitiveEdges = () => {
 		{
 			id: 2,
 			title: "Entry-to-communication",
-			icon: <BarChart3 className="w-12 h-12 text-white" />,
+			icon: <BarChart3 className="w-12 h-12 text-[#0d1b3e]" />,
 			bgColor: "bg-[#0d1b3e]",
-			textColor: "text-white",
+			textColor: "text-[#0d1b3e]",
 			borderColor: "border-[#0d1b3e]",
 			hover: "hover:bg-linear-to-r from-[#00A9C0] to-[#7CD957] hover:text-white transition-colors duration-300 ease-in-out",
 			isActive: false,
@@ -53,9 +53,9 @@ const CompetitiveEdges = () => {
 		{
 			id: 3,
 			title: "Cost efficiency",
-			icon: <DollarSign className="w-12 h-12 text-white" />,
+			icon: <DollarSign className="w-12 h-12 text-[#0d1b3e]" />,
 			bgColor: "bg-white",
-			textColor: "text-white",
+			textColor: "text-[#0d1b3e]",
 			borderColor: "border-white",
 			hover: "hover:bg-linear-to-r from-[#00A9C0] to-[#7CD957] hover:text-white transition-colors duration-300 ease-in-out",
 			isActive: false,
@@ -69,8 +69,8 @@ const CompetitiveEdges = () => {
 	];
 
 	return (
-		<section className="w-full py-16 px-4 text-white">
-			<div className="max-w-5xl mx-auto">
+		<section className="w-full text-white">
+			<div className="mx-auto">
 				<div className="text-center mb-12">
 					<h2 className="text-3xl md:text-4xl font-bold mb-2 text-[#001159] dark:text-white">
 						OUR COMPETITIVE EDGES
@@ -80,17 +80,55 @@ const CompetitiveEdges = () => {
 					</p>
 				</div>
 
-				<div className="grid grid-cols-1 md:grid-cols-4 gap-6 mb-10">
+				<div className="grid grid-cols-1 md:grid-cols-4 gap-16 mb-10">
 					{edges.map((edge, index) => (
 						<div
 							key={index}
-							className="flex flex-col"
+							className="flex flex-col float-up"
+							style={{ animationDelay: `${index * 0.1}s` }}
 						>
-							<div
-								className={`w-full aspect-square bg-[#0d1b3e] ${edge.hover} flex flex-col items-end justify-between rounded-lg shadow-lg p-4`}
-							>
-								<p className={`text-2xl w-full ${edge.textColor}`}>{edge.title}</p>
-								{edge.icon}
+							<div className="group relative w-full aspect-square bg-white flex flex-col items-end justify-between shadow-lg p-4 overflow-hidden">
+								<div className="absolute inset-0 pointer-events-none overflow-hidden">
+									<div
+										className="absolute top-0 left-0 w-0 h-[2px] group-hover:w-full transition-all duration-300 ease-in-out"
+										style={{
+											backgroundImage:
+												"linear-gradient(to right, #00A9C0, #7CD957)",
+										}}
+									></div>
+
+									<div
+										className="absolute top-0 right-0 w-[2px] h-0 group-hover:h-full transition-all duration-300 ease-in-out"
+										style={{
+											backgroundImage:
+												"linear-gradient(to bottom, #00A9C0, #7CD957)",
+											transitionDelay: "100ms",
+										}}
+									></div>
+
+									<div
+										className="absolute bottom-0 right-0 w-0 h-[2px] group-hover:w-full transition-all duration-300 ease-in-out"
+										style={{
+											backgroundImage:
+												"linear-gradient(to left, #00A9C0, #7CD957)",
+											transitionDelay: "200ms",
+										}}
+									></div>
+
+									<div
+										className="absolute bottom-0 left-0 w-[2px] h-0 group-hover:h-full transition-all duration-300 ease-in-out"
+										style={{
+											backgroundImage:
+												"linear-gradient(to top, #00A9C0, #7CD957)",
+											transitionDelay: "300ms",
+										}}
+									></div>
+								</div>
+
+								<p className={`text-2xl w-full ${edge.textColor} relative z-10`}>
+									{edge.title}
+								</p>
+								<div className="relative z-10">{edge.icon}</div>
 							</div>
 						</div>
 					))}
@@ -98,11 +136,11 @@ const CompetitiveEdges = () => {
 
 				<div className="w-[90px] h-[8px] bg-linear-to-r from-[#00A9C0] to-[#7CD957] mb-4"></div>
 
-				<div className="space-y-2 text-gray-200 pl-4 md:pl-0">
+				<div className=" text-gray-200 pl-4 md:pl-0">
 					{edges[0].desription.map((benefit, index) => (
 						<p
 							key={index}
-							className="leading-relaxed text-gray-700 dark:text-white"
+							className="text-gray-700 dark:text-white text-xl md:text-2xl font-light leading-relaxed"
 						>
 							{benefit}
 						</p>

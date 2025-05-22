75530f566884e35a4c20d48ee7422230fa30021c
LEMONS\anhch <anhchanhung@gmail.com>
2025-05-22 17:22:19 +0700

    update UI

--- src/layouts/components/Footer.jsx
+++ src/layouts/components/Footer.jsx
@@ -99,10 +99,12 @@ const Footer = () => {
 			<div className="container mx-auto max-w-[1440px] pt-12 border-t border-gray-100">
 				<div className="flex flex-col md:flex-row-reverse">
 					{/* T2Soft */}
-
+					{/* Contact info */}
 					<div className="md:w-3/8 mb-8 md:mb-0 flex flex-col items-end space-y-6">
-						{/* Contact info */}
 						<div className="space-y-3">
+							<h3 className="text-foreground justify-start font-semibold text-lg mb-4">
+								Contact
+							</h3>
 							{CONTACT_ITEMS.map((item, index) => (
 								<div
 									key={index}
@@ -112,23 +114,6 @@ const Footer = () => {
 									{item.icon}
 									{item.content}
 								</div>
-							))}
-						</div>
-
-						{/* Social links */}
-
-						<div className="flex space-x-3">
-							{SOCIAL_LINKS.map((social, index) => (
-								<a
-									key={index}
-									href={social.url}
-									target="_blank"
-									rel="noopener noreferrer"
-									className={`${social.bgColor} ${social.textColor} p-2 rounded-full w-8 h-8 flex items-center justify-center hover:opacity-90 transition-opacity`}
-									aria-label={social.name}
-								>
-									{social.icon}
-								</a>
 							))}
 						</div>
 					</div>
@@ -160,21 +145,35 @@ const Footer = () => {
 					</div>
 
 					<div className="md:w-2/8 mb-8 md:mb-0 flex flex-col space-y-6">
-						<div>
-							<Link
-								to="/"
-								className="text-foreground font-bold text-2xl"
-							>
-								T2Soft
-							</Link>
-							<p className="text-foreground/80 text-sm mt-2">
-								Your global software service and solution partner
-							</p>
+						<Link
+							to="/"
+							className="text-foreground font-bold text-2xl"
+						>
+							T2Soft
+						</Link>
+						<p className="text-foreground/80 text-sm mt-2">
+							Your global software service and solution partner
+						</p>
+						{/* Social links */}
+
+						<div className="flex space-x-3">
+							{SOCIAL_LINKS.map((social, index) => (
+								<a
+									key={index}
+									href={social.url}
+									target="_blank"
+									rel="noopener noreferrer"
+									className={`${social.bgColor} ${social.textColor} p-2 rounded-full w-8 h-8 flex items-center justify-center hover:opacity-90 transition-opacity`}
+									aria-label={social.name}
+								>
+									{social.icon}
+								</a>
+							))}
 						</div>
 					</div>
 				</div>
 
-				<div className="w-full mt-12 pt-4 border-t border-gray-100 text-center">
+				<div className="w-full mt-6 p-4 border-t flex justify-center border-gray-100 text-center">
 					<p className="text-foreground/60 text-sm">
 						© {currentYear} T2Soft. All rights reserved.
 					</p>
--- src/views/Home/components/SideNavigation.jsx
+++ src/views/Home/components/SideNavigation.jsx
@@ -62,22 +62,22 @@ const SideNavigation = () => {
 				<div key={section.id}>
 					{idx !== 0 && (
 						<div className="flex justify-center w-full">
-							<div className="w-px h-8 bg-light-blue" />
+							<div className="w-px h-8 bg-foreground/50" />
 						</div>
 					)}
 					<div className="relative group flex items-center justify-center">
 						<button
 							onClick={() => scrollToSection(section.id)}
-							className={`w-7 h-7 text-sm font-bold rounded-full text -tracking-wider flex items-center justify-center cursor-pointer transition-colors border-1 border-light-blue hover:text-white ${
+							className={`w-7 h-7 text-sm font-bold rounded-full text -tracking-wider flex items-center justify-center cursor-pointer transition-colors border-1 border-foreground hover:text-white ${
 								activeSection === section.id
-									? "bg-light-blue text-slate-200 text-light"
-									: "bg-white text-light-blue  hover:bg-light-blue"
+									? "bg-foreground text-slate-200 text-light"
+									: "bg-white text-light  hover:bg-foreground"
 							}`}
 							aria-label={`Go to ${section.id}`}
 						>
 							{section.label}
 						</button>
-						<span className="absolute left-9 capitalize  bg-light-blue text-white text-xs px-2 py-1 rounded-md whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
+						<span className="absolute left-9 capitalize  bg-foreground text-white text-xs px-2 py-1 rounded-md whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none">
 							{section.id.replace(/-/g, " ")}
 						</span>
 					</div>

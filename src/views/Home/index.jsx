f5d08ffa32ae35e92721a3ee07b4b4eb157d9648
fushiyin <trucongftu@gmail.com>
2025-06-03 18:04:38 +0900

    merge

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,7 +1,6 @@
 import CTA from "@/components/sections/ContactCTA";
 import { HEADER_STYLE } from "@/constant/header";
 import { SECTIONS_KEY } from "@/constant/sideNavigation";
-import VisionJourney2 from "./Demo";
 import CompetitiveEdges from "./components/CompetitiveEdges";
 import DevelopmentCapacity from "./components/DevelopmentCapacity/index";
 import FocusedIndustries from "./components/FocusedIndustries";
@@ -10,6 +9,7 @@ import OurProcess from "./components/OurProcess";
 import SideNavigation from "./components/SideNavigation";
 import Testimonials from "./components/Testimonials";
 import WhyVietnam from "./components/WhyVietnam";
+import VisionJourney from "./Vision";
 
 function HomePage() {
 	const sectionClass = "flex items-center justify-center relative overflow-hidden";
@@ -32,7 +32,7 @@ function HomePage() {
 				id={SECTIONS_KEY.VISION.id}
 				className={sectionClass}
 			>
-				<VisionJourney2 />
+				<VisionJourney />
 			</section>
 			<section
 				id={SECTIONS_KEY.COMPETITIVE_EDGES.id}

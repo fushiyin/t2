32b8010415078228a1f42546831995bec921be93
LEMONS\anhch <anhchanhung@gmail.com>
2025-06-18 17:27:59 +0700

    update code

--- src/layouts/components/Header.jsx
+++ src/layouts/components/Header.jsx
@@ -293,7 +293,7 @@ const Header = () => {
 						className={classNames(
 							"md:hidden p-2 rounded-md hover:bg-gray-100 dark:hover:bg-gray-700",
 							{
-								"text-white": !isScrolled && isMobile,
+								"text-white": !isScrolled && isMobile && isHome,
 								"text-gray-700 dark:text-gray-200": isScrolled,
 							},
 						)}
